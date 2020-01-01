c9b11f67e74360f09f33da599d37d6e86796a490
Adrienne Miranda <adrienne.c.miranda@gmail.com>
2020-01-01 20:48:13 +0000

    Automatically backed up by Learn

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ router.get('/', (request, response) => {
   response.end();https://learn.co/tracks/introduction-to-node-js/intro-to-node-js/async-i-o/building-a-basic-web-server#
 });
 
+route.post('/message', (request, response) => {
+  // Save the message and send the message id back to the client.
+});
+
 const server = http.createServer((request, response) => {
   router(request, response, finalhandler(request, response));
 });
